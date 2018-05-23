41bf2540ad3cb41ca594c22935259315b0e6d3a9
Raza Mian <raza.mian@yahoo.com>
2018-05-22 19:31:28 -0500

    first commit

---
+++ Data.cs
@@ -0,0 +1,338 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+
+namespace LiquidusData
+{
+    public sealed class Data
+    {
+        string connectionString;
+        SqlCommand sqlCmd; 
+
+        /// <summary>
+        /// A library for updating or returning records from the database.
+        /// </summary>
+        public Data()
+        {
+            connectionString = "";
+            sqlCmd = new SqlCommand();
+        }
+        
+        /// <summary>
+        /// A library for updating or returning records from the database.
+        /// </summary>
+        /// <param name="ConnectionString">The connections string for the database.</param>
+        public Data(string ConnectionString)
+        {
+            connectionString = ConnectionString;
+            sqlCmd = new SqlCommand();
+        }
+
+
+        /// <summary>
+        /// Adds a parameter to the query command.
+        /// </summary>
+        /// <param name="ParameterName">The parameter name.</param>
+        /// <param name="value">The parameter's value.</param>
+        public void AddParameter(string ParameterName, object value)
+        {
+            SqlParameter param = new SqlParameter();
+
+            param.ParameterName = ParameterName;
+            param.Value = value;
+            sqlCmd.Parameters.Add(param);
+
+        }
+
+        /// <summary>
+        /// Adds a parameter to the query command, and allows you to specify the direction.
+        /// </summary>
+        /// <param name="ParameterName">The parameter name.</param>
+        /// <param name="value">The parameter's value.</param>
+        /// <param name="Direction"></param>
+        /// <remarks></remarks>
+
+        public void AddParameter(string ParameterName, object value, ParameterDirection Direction)
+        {
+            SqlParameter param = new SqlParameter();
+
+            param.ParameterName = ParameterName;
+            param.Value = value;
+            param.Direction = Direction;
+            sqlCmd.Parameters.Add(param);
+
+        }
+        /// <summary>
+        /// Wipes out all parameters in query command.
+        /// </summary>
+        public void ClearParameter()
+        {
+            if ((sqlCmd.Parameters == null) == false)
+            {
+                sqlCmd.Parameters.Clear();
+            }
+        }
+
+        /// <summary>
+        /// Removes a parameter from the sql command.
+        /// </summary>
+        /// <param name="ParameterName">The parameter to remove.</param>
+        public void ClearParameter(string ParameterName)
+        {
+            if ((sqlCmd.Parameters == null) == false)
+            {
+                SqlParameter param = sqlCmd.Parameters[ParameterName];
+                sqlCmd.Parameters.Remove(param);
+            }
+        }
+
+        /// <summary>
+        /// Creates a data table
+        /// </summary>
+        /// <param name="SQLStatement">The stored procedure you are calling.</param>
+        /// <returns>A shiny new data table.</returns>
+        /// <remarks></remarks>
+        public DataTable GetDataTable(string SQLStatement)
+        {
+            DataTable dt = default(DataTable);
+
+            using (SqlConnection cn = new SqlConnection(connectionString))
+            {
+                cn.Open();
+                sqlCmd.Connection = cn;
+                sqlCmd.CommandText = SQLStatement;
+
+                sqlCmd.CommandType = CommandType.StoredProcedure;
+                sqlCmd.CommandTimeout = 0;
+
+                using (DataSet QueryData = new DataSet())
+                {
+                    using (SqlDataAdapter QueryAdapter = new SqlDataAdapter(sqlCmd))
+                    {
+                        try
+                        {
+                            QueryAdapter.Fill(QueryData);
+                        }
+                        catch (Exception ex)
+                        {
+                            this.WriteError(ex);
+                        }
+                    }
+
+                    try
+                    {
+                        dt = QueryData.Tables[0];
+                    }
+                    catch (Exception ex)
+                    {
+                        this.WriteError(ex);
+                    }
+                }                                
+                cn.Close();
+            }
+            
+            return dt;
+        }
+
+        /// <summary>
+        /// Executes a non-query statement.  
+        /// </summary>
+        /// <param name="SQLStatement">The stored procedure to run.</param>
+        /// <returns>A boolean stating if the statement was successful or not.</returns>        
+        public bool ExecuteNonQuery(string SQLStatement)
+        {
+            bool SuccessfulExecution = true;
+
+            using (SqlConnection cn = new SqlConnection(connectionString))
+            {
+                cn.Open();
+                sqlCmd.Connection = cn;
+                sqlCmd.CommandText = SQLStatement;
+                sqlCmd.CommandType = CommandType.StoredProcedure;
+                sqlCmd.CommandTimeout = 0;
+
+                using (SqlDataAdapter QueryAdapter = new SqlDataAdapter(sqlCmd))
+                {
+                    try
+                    {
+                        sqlCmd.ExecuteNonQuery();
+                    }
+                    catch (Exception ex)
+                    {
+                        Trace.WriteLine(String.Concat(SQLStatement, " --- ", connectionString));
+                        Trace.WriteLine(ex);
+                        this.WriteError(ex);
+                        SuccessfulExecution = false;
+                    }
+                    finally
+                    {
+                        sqlCmd.Connection.Close();
+                    }
+                }
+                cn.Close();
+            }
+            
+            //tell the calling function if this was successful
+            return SuccessfulExecution;
+
+        }
+
+        /// <summary>
+        /// Executes a scalar command and returns the the first column of the first row in the result set returned by the query.
+        /// </summary>
+        /// <param name="SQLStatement">The stored procedure to run.</param>
+        /// <returns>The first column of the first row in the result set returned by the query.</returns>
+        public object ExecuteScalar(string SQLStatement)
+        {
+
+            object iIdentity = null;
+
+            using (SqlConnection cn = new SqlConnection(connectionString))
+            {
+                cn.Open();
+                sqlCmd.Connection = cn;
+                sqlCmd.CommandText = SQLStatement;
+                sqlCmd.CommandType = CommandType.StoredProcedure;
+                sqlCmd.CommandTimeout = 0;
+
+                using (SqlDataAdapter QueryAdapter = new SqlDataAdapter(sqlCmd))
+                {
+                    try
+                    {
+                        iIdentity = sqlCmd.ExecuteScalar();
+                    }
+                    catch (Exception ex)
+                    {
+                        this.WriteError(ex);
+                        iIdentity = 0;
+                    }
+                    finally
+                    {
+                        sqlCmd.Connection.Close();
+                    }
+                }
+                cn.Close();
+            }
+
+            return iIdentity;
+        }
+
+        /// <summary>
+        /// Returns the schema of a table.
+        /// </summary>
+        /// <param name="tablename">The table to check.</param>        
+        public DataTable GetDataTableSchema(string tablename)
+        {
+
+            DataTable tbl = new DataTable();
+            string selectStatement = string.Concat("SELECT TOP 1 * FROM ", tablename);
+            
+            using (SqlConnection cn = new SqlConnection(connectionString))
+            {
+                cn.Open();
+                var _with1 = sqlCmd;
+                _with1.Connection = cn;
+                _with1.CommandType = CommandType.Text;
+                _with1.CommandText = selectStatement;
+                _with1.CommandTimeout = 0;
+
+                using (DataSet QueryData = new DataSet())
+                {
+                    using (SqlDataAdapter QueryAdapter = new SqlDataAdapter(sqlCmd))
+                    {
+                        try
+                        {
+                            QueryAdapter.Fill(QueryData);
+                        }
+                        catch (Exception ex)
+                        {
+                            this.WriteError(ex);
+                        }
+                    }
+
+                    try
+                    {
+                        tbl = QueryData.Tables[0];
+                    }
+                    catch (Exception ex)
+                    {
+                        tbl = new DataTable();
+                        this.WriteError(ex);
+                    }
+                }
+
+                cn.Close();
+            }
+            return tbl;
+
+        }
+
+        /// <summary>
+        /// Creates a data table
+        /// </summary>
+        /// <param name="SQLStatement">The stored procedure you are calling.</param>
+        /// <returns>A shiny new data table.</returns>
+        /// <remarks></remarks>
+        public DataSet GetDataSet(string SQLStatement)
+        {
+            DataSet ds = default(DataSet);
+
+            using (SqlConnection cn = new SqlConnection(connectionString))
+            {
+                cn.Open();
+                sqlCmd.Connection = cn;
+                sqlCmd.CommandText = SQLStatement;
+
+                sqlCmd.CommandType = CommandType.StoredProcedure;
+                sqlCmd.CommandTimeout = 0;
+
+                using (ds = new DataSet())
+                {
+                    using (SqlDataAdapter QueryAdapter = new SqlDataAdapter(sqlCmd))
+                    {
+                        try
+                        {
+                            QueryAdapter.Fill(ds);
+                        }
+                        catch (Exception ex)
+                        {
+                            this.WriteError(new Exception(SQLStatement));
+                            this.WriteError(ex);
+                        }
+                    }
+                }
+                cn.Close();
+            }
+
+            return ds;
+        }
+
+        /// <summary>
+        /// Sends the exception message to the status handler.
+        /// </summary>
+        /// <param name="ex">The exception </param>
+        private void WriteError(Exception ex)
+        {
+            Trace.WriteLine(ex);
+        }
+
+        //event handlers
+        #region
+        public delegate void WriteStatusHandler(string Status);
+        public event WriteStatusHandler StatusUpdate;
+        public void WriteStatus(string Status)
+        {
+            if (StatusUpdate != null)
+                StatusUpdate(Status);
+        }
+        
+        #endregion
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,231 @@
+﻿using LiquidusData;
+using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DDDImport
+{
+    class Program
+    {
+        private static string ConnectionString;
+
+        public enum DealerListingUsageAll
+        {
+            DealerID,
+            StockNumber,
+            InventoryDate,
+            Type, //Type_NewOrUsed,
+            Status,
+            InvoicePrice,
+            PackAmount,
+            Cost,
+            price, //ListPrice,
+            MSRP,
+            LotLocation,
+            Condition,
+            Tagline,
+            IsCpo, //IsCertified,
+            CertificationNumber,
+            vin, //VIN,
+            make, //Make,
+            model, //Model,
+            year, //ModelYear,
+            ModelCode,
+            Trim, //TrimLevel,
+            SubTrimLevel,
+            Classification,
+            VehicleTypeCode,
+            miles, //Odometer,
+            PayloadCapacity,
+            SeatingCapacity,
+            WheelBase,
+            bodyStyle, //BodyDescription,
+            BodyDoorCount,
+            DriveTrainDescription,
+            EngineDesc, //EngineDescription,
+            EngineCylinderCount,
+            TransDesc, //TransmissionDescription,
+            TransmissionType,
+            ExtColor, //ExteriorColorDescription,
+            ExteriorColorBase,
+            InteriorDescription,
+            InteriorColor,
+            StandardFeatures, //pipe delimited multi-value
+            DealerFeatures, //DealerAddedFeatures, //pipe delimited multi-value
+            TechnicalFeatures,
+            SafetyFeatures,
+            InteriorFeatures,
+            ExteriorFeatures,
+            ElectronicFeatures,
+            OtherFeatures,
+            CreatedDate,
+            LastModifiedDate,
+            ModifiedFlag,
+            DealerName,
+            DealerAddress,
+            DealerCity,
+            DealerState,
+            DealerPostalCode,
+            DealerPhoneNumber,
+            mid, //MediaID,
+            thumbnail, //ImageURLs,
+            URL, //VehicleURl
+        }
+
+        //if you want to update more file fields to the database, just put the field name in the enum below and above in the order the field appears in the file
+        public enum DealerListingUsageForDDD
+        {
+            DealerID,
+            StockNumber,
+            Type,
+            price,
+            IsCpo,
+            vin,
+            make,
+            model,
+            year,
+            Trim,
+            miles,
+            bodyStyle,
+            EngineDesc,
+            TransDesc,
+            ExtColor,
+            DealerFeatures,  
+            TechnicalFeatures,
+            SafetyFeatures,
+            InteriorFeatures,
+            ExteriorFeatures,
+            ElectronicFeatures,
+            OtherFeatures,
+            mid,
+            thumbnail,
+            URL
+        }
+
+        static void Main(string[] args)
+        {
+            ConnectionString = ConfigurationManager.ConnectionStrings["ApplicationConnectionString"].ConnectionString.ToString();
+
+            Data ldCampaigns = new Data(ConnectionString);
+            DataTable dt = ldCampaigns.GetDataTable("CampaignGet");
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                int CampaignID = (int)dr["CampaignID"];
+                DoCampaign(CampaignID);
+            }
+        }
+
+        private static void DoCampaign(int campaignID)
+        {
+            Data ldDealer = new Data(ConnectionString);
+            ldDealer.AddParameter("CampaignID", campaignID);
+            DataTable dt = ldDealer.GetDataTable("CampaignDealerGet");
+            foreach (DataRow dr in dt.Rows)
+            {
+                int DealerID = (int)dr["DealerID"];
+                int SourceID = (int)dr["SourceID"];
+                string SourceDealerID = dr["SourceDealerID"].ToString();
+                DoDealer(DealerID, campaignID, SourceDealerID);
+            }
+        }
+
+        private static void DoDealer(int dealerID, int CampaignID, string SourceDealerID)
+        {
+            // Clear old
+            Data ldClear = new Data(ConnectionString);
+            ldClear.AddParameter("CampaignID", CampaignID);
+            ldClear.AddParameter("DealerID", dealerID);
+            ldClear.AddParameter("Delete", 1);
+            ldClear.ExecuteNonQuery("CampaignDealerListingSet");
+
+            string DealerPath = string.Format(ConfigurationManager.AppSettings.Get("DealerPath")+"{0}.txt", SourceDealerID); 
+            if (File.Exists(DealerPath))
+            {
+                using (TextFieldParser DMiReader = new TextFieldParser(DealerPath))
+                {
+                    DMiReader.TextFieldType = FieldType.Delimited;
+                    DMiReader.Delimiters = new string[] { "\t" }; //just set the column delimeter (tab)
+                    DMiReader.HasFieldsEnclosedInQuotes = false;
+
+                    //read each row (since parser is automatically newline delimeted)
+                    while (!DMiReader.EndOfData)
+                    {
+                        List<KeyValuePair<string, object>> sqlParams = new List<KeyValuePair<string, object>>();
+
+                        //parse row for columns
+                        string[] Listing = DMiReader.ReadFields();
+
+                        //for each column
+                        for (int clmIdx = 0; clmIdx < Listing.Length; clmIdx++)
+                        {
+                            if (clmIdx == 0)
+                            {
+                                KeyValuePair<string, object> paramAdditional = new KeyValuePair<string, object>("DealerID", dealerID);
+                                sqlParams.Add(paramAdditional);
+                            }
+                            else
+                            {
+                                //generate parameters for sproc
+                                KeyValuePair<string, object> param = new KeyValuePair<string, object>(((DealerListingUsageAll)clmIdx).ToString(), Listing[clmIdx]);
+                                if (Enum.GetNames(typeof(DealerListingUsageForDDD)).Contains(param.Key))
+                                    if (param.Key != "thumbnail" && param.Key != "price")
+                                        sqlParams.Add(param);
+
+                                //add this param manually because the file value needs to be duplicated for another param for "vid"
+                                if (param.Key == "StockNumber")
+                                {
+                                    KeyValuePair<string, object> paramAdditional = new KeyValuePair<string, object>("vid", Listing[clmIdx]);
+                                    sqlParams.Add(paramAdditional);
+                                }
+
+                                //also do this manually because the file value needs to be duplicated for another param for "ImageList" and the original ("thumbnail") needs to be modified
+                                if (param.Key == "thumbnail")
+                                {
+                                    KeyValuePair<string, object> paramModified = new KeyValuePair<string, object>("thumbnail", Listing[clmIdx].Split('|').FirstOrDefault());
+                                    sqlParams.Add(paramModified);
+                                    KeyValuePair<string, object> paramAdditional = new KeyValuePair<string, object>("ImageList", Listing[clmIdx]);
+                                    sqlParams.Add(paramAdditional);
+                                }
+
+                                //format price from ##.00 to without decimals as sproc param is int
+                                if (param.Key == "price")
+                                {
+                                    string priceInFile = Listing[clmIdx];
+
+                                    double priceForDB = 0;
+
+                                    if (!string.IsNullOrEmpty(priceInFile))
+                                       priceForDB =  double.Parse(priceInFile.Split(',')[0]);
+
+                                    KeyValuePair<string, object> paramAdditional = new KeyValuePair<string, object>("price", priceForDB);
+                                    sqlParams.Add(paramAdditional);
+                                }
+                            }
+                        }
+
+                        Data ldListingSet = new Data(ConnectionString);
+                        ldListingSet.AddParameter("CampaignID", CampaignID);
+
+                        //add the rest of the params
+                        foreach (KeyValuePair<string, object> par in sqlParams)
+                            ldListingSet.AddParameter(par.Key, par.Value);
+
+                        //call the sproc finally
+                        ldListingSet.ExecuteNonQuery("CampaignDealerListingSet");
+
+                        ldListingSet.ClearParameter();
+                    }
+                }
+            }
+        }
+    }
+}
+
